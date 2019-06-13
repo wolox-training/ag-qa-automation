3afe03d47525d1722780509e6f1f2a87a2ad6202
guerragomez <andres.guerra@wolox.co>
2019-06-13 00:22:37 -0500

    add new step definitions

--- features/search.feature
+++ features/search.feature
@@ -2,5 +2,7 @@
 Feature: Search
 
 Scenario: Search Amazon 
-When when I search in Amazon "iphone case"
+When when I search "iphone case"
+And Filter for "Polycarbonate"
+And Order by price "Price: High to Low"
 
--- features/step_definitions/home.rb
+++ features/step_definitions/home.rb
@@ -9,5 +9,16 @@ When 'when I search {string}' do |value|
   wait_for_element_to_display(:xpath, '//input[@value="Go"]', 10)
   enter_text(:id, value, 'twotabsearchtextbox')
   click(:xpath, '//input[@value="Go"]')
-  sleep 3
+  sleep 2
+end
+
+And 'Filter for {string}' do |filter|
+  wait_for_element_to_display(:xpath, '//span[@class="a-size-base a-color-base"][contains(.,"Polycarbonate")]', 10)
+  click(:xpath, '//span[@class="a-size-base a-color-base"][contains(.,"Polycarbonate")]')
+end
+
+And 'Order by price {string}' do |item_drop|
+  wait_for_element_to_display(:id, 's-result-sort-select', 10)
+  select_option_from_dropdown(:id, :text, item_drop, 's-result-sort-select')
+  sleep 10
 end
